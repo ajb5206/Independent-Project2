a853b1b8f31b9d153a07bdefaad492ddbcbc945d
Alex Bertotto <ajb5206@gmail.com>
2021-06-19 16:00:05 -0700

    add question 3 to the html file

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,36 @@
 $(document).ready(function() {
 	$("#formOne").submit(function(event) {
 		const question1Input = $("input#question1").val();
+		const question2Input = $("input#question2").val();
 
 		$(".question1").text(question1Input);
+		$(".question2").text(question2Input);
+
 
 		let count = 0;
-		if (question1 === front-end) {
+		if (".question1" === "front-end") {
 			count = count +1;
 		} else {
 			count = count -1;
 		};
 
-		if (count > 0) {
+
+		if (".question2" === "user interface") {
+			count = count +1;
+		} else {
+			count = count-1;
+		}
+
+		if (count >= 1) {
 			$("#javascript").show();
-		} else if (count = 0) {
+		} else if (count === 0) {
 			$("#python").show();
 		} else {
 			$("#cSharp").show();
 		}
 
+		$("#count").show();
+
 		event.preventDefault();
 	});
-})
+});
