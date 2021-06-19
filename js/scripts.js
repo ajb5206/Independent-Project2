f7d3e14a8f6cb20336695c1e3f9c8cd91c5c79cc
Alex Bertotto <ajb5206@gmail.com>
2021-06-19 13:30:33 -0700

    add rudimentary branching counter

--- js/scripts.js
+++ js/scripts.js
@@ -3,5 +3,22 @@ $(document).ready(function() {
 		const question1Input = $("input#question1").val();
 
 		$(".question1").text(question1Input);
-	})
+
+		let count = 0;
+		if (question1 === front-end) {
+			count = count +1;
+		} else {
+			count = count -1;
+		};
+
+		if (count > 0) {
+			$("#javascript").show();
+		} else if (count = 0) {
+			$("#python").show();
+		} else {
+			$("#cSharp").show();
+		}
+
+		event.preventDefault();
+	});
 })
